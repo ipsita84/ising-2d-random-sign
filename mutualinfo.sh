6be56943dd1abee4ec1bf5846b1218df88d9bd3e
Ipsita Mandal <ipsita.mandal@gmail.com>
2014-10-21 09:55:25 -0400

    bashscrift & makefile added

---
+++ mutualinfo.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+test ${#} -ne 3 && (echo "Insufficient number of arguments: 3 expected, got ${#}"; exit 1)
+
+xmin=${1}
+xmax=${2}
+xdel=${3}
+xmaxby2=$(bc <<< "scale=3; ${xmax}/2")
+
+bindis=../disorder
+binnor=../normal
+binrep=../replica
+binmut=../mutualinfo
+
+dirname=`date +%F-%H-%M-%S`
+echo
+echo "Mutual-Info calculator"
+echo "============================================================"
+dirtry=1
+while [ -d ${dirname} ]; do
+  dirname=${dirname}-${dirtry}
+  echo "Directory with same name exists, using different dir name ${dirname}"
+done
+
+echo ""
+echo "Generating executables..."
+make all
+echo "All executables generated."
+
+mkdir ${dirname}
+pushd ${dirname}
+
+echo
+echo "Generating disorder realisation: ${bindis}..."
+${bindis}
+echo "${bindis} done."
+
+echo
+echo "Running ${binnor} from ${xmin}--${xmax} with ${xdel} increments..."
+${binnor} ${xmin} ${xmax} ${xdel}
+echo "${binnor} done."
+
+echo
+echo "Running ${binrep} from ${xmin}--${xmaxby2} with ${xdel} increments..."
+${binrep} ${xmin} ${xmaxby2} ${xdel}
+echo "${binrep} done."
+
+echo
+echo "Computing mutual-info..."
+${binmut} ${xmin} ${xmaxby2} ${xdel}
+echo "${binmut} done."
+
+sleep 2
+
+echo
+echo "All done. Bye!"
+echo "============================================================"
+
+popd
