d3381aaa4d8fbe6df0a7abc2d6b5eae08fe94d9f
ipsita84 <ipsita.mandal@gmail.com>
2015-02-03 08:10:53 -0700

    Update EmA-vs-beta.cc

--- EmA-vs-beta.cc
+++ EmA-vs-beta.cc
@@ -1,4 +1,4 @@
-//  g++ -Wall -O3 Em-vs-beta-Replica-sim-anl2.cc -o replica
+//  g++ -Wall -O3 EmA-vs-beta.cc -o replica
 // Run with command line arguments, e.g. ./testo betamin betamax delbeta
 
 // Energy for replicated A -- gives S_2(A)
