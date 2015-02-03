9e7854a663cf40c97e4197eef41d351d25bed242
Ipsita mandal <ipsita@linux.site>
2015-02-03 07:46:18 -0700

    computing S2(A),S2(B);rnd disorder->S2(A)\neqS2(B)

--- Mutual-info-vs-beta.cc
+++ Mutual-info-vs-beta.cc
@@ -4,6 +4,15 @@
 //Parameters that can be changed for different runs:
 //J, axis1, axis2, N_mc
 
+// S_2 (A)= 2 ln(Z[T])-ln(Z[A,2,T])
+// S_2 (B)= 2 ln(Z[T])-ln(Z[B,2,T])
+// S_2 (A U B)= 2 ln(Z[T])-ln(Z[T/2])
+// I_2 (A; B) = S_2 (A) + S_2 (B) - S_2 (A U B) 
+// = 2 ln(Z[T])+ln(Z[T/2])-ln(Z[A,2,T])-ln(Z[B,2,T])
+// =-2\int_0^{\beta} E - \int_0^{2 \beta} E 
+//   + \int_0^{\beta} E_replica_A + \int_0^{\beta} E_replica_B
+
+
 #include <iostream>
 #include <fstream>
 #include <ctime>
@@ -63,8 +72,13 @@ int main(int argc, char const * argv[])
 //	cin >> del_beta;
 	double mut_info(0); //mutual information I_2
 	ofstream fout("I2.dat"); // Opens a file for output
-	vvdouble vm = tabdatr("Em.dat", 2);//modified energy data
-	interp_data idm(vm,1);
+
+	vvdouble vmA = tabdatr("EmA.dat", 2);//modified energy data for A
+	interp_data idm(vmA,1);
+
+	vvdouble vmB = tabdatr("EmB.dat", 2);//modified energy data for B
+	interp_data idm(vmB,1);
+
 	vvdouble vn = tabdatr("E.dat", 2);//normal energy data
 	interp_data idn(vn,1);
 //	gsl_integration_workspace * w
@@ -76,21 +90,29 @@ int main(int argc, char const * argv[])
 	for (double beta = beta_min; beta < beta_max + del_beta; beta += del_beta)
 	{
 		mut_info = 0 ;
-		double term1(0), term2(0), term3(0), abs_error(0);
+		double int_E_T(0), int_E_Tby2(0),int_EA_T(0),int_EB_T(0),abs_error(0);
 		gsl_function F;
 		F.function = &f;
 		F.params = &idn;
 //Function: int gsl_integration_qags (const gsl_function * f,double a,double b, double epsabs, double epsrel,size_t limit,gsl_integration_workspace * workspace,double * result, double *abserr)
 //gsl_integration_cquad (const gsl_function * f, double a, double b, double epsabs, double epsrel, gsl_integration_cquad_workspace * workspace, double * result, double * abserr, size_t * nevals)
 		gsl_integration_cquad (&F, 0,     beta, 1e-6, 1e-4,
-		                       w, &term2, &abs_error, &nevals);
+		                       w, &int_E_T, &abs_error, &nevals);
 		gsl_integration_cquad (&F, 0, 2.0*beta, 1e-6, 1e-4,
-		                       w, &term3, &abs_error, &nevals);
+		                       w, &int_E_Tby2, &abs_error, &nevals);
 		F.function = &g;
-		F.params = &idm;
-		gsl_integration_cquad (&F, 0, beta, 1e-6, 1e-4, w, &term1,
+		F.params = &idmA;
+		gsl_integration_cquad (&F, 0, beta, 1e-6, 1e-4, w, &int_EA_T,
 		                       &abs_error, &nevals);
-		mut_info =2.0*term1 -2.0* term2 - term3;
+
+		F.function = &h;
+		F.params = &idmB;
+		gsl_integration_cquad (&F, 0, beta, 1e-6, 1e-4, w, &int_EB_T,
+		                       &abs_error, &nevals);
+// I2 =-2\int_0^{\beta} E - \int_0^{2 \beta} E 
+//   + \int_0^{\beta} E_replica_A + \int_0^{\beta} E_replica_B
+
+                mut_info = int_EA_T + int_EB_T - int_E_T - int_E_Tby2
 		fout << beta << '\t' << mut_info /axis2 << endl;
 	}
 
@@ -110,6 +132,13 @@ double g (double beta, void * params)
 	interp_data p = *(interp_data *) params;
 	return p.interp_akima(beta);
 }
+
+double h (double beta, void * params)
+{
+	interp_data p = *(interp_data *) params;
+	return p.interp_akima(beta);
+}
+
 
 
 
